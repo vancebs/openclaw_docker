0a2f88cefe6e6a8abf28b21067b46be66b4e2f48
vancebs <vancebs@qq.com>
2026-04-28 18:36:22 +0800

    remove multica

--- restart.sh
+++ restart.sh
@@ -3,6 +3,7 @@
 source utils.sh
 pushd ${DOCKER_DIR} > /dev/null 2>&1
 
+echo "==> Restarting openclaw ..."
 docker_compose restart
 
 popd > /dev/null 2>&1
--- stop.sh
+++ stop.sh
@@ -3,6 +3,7 @@
 source utils.sh
 pushd ${DOCKER_DIR} > /dev/null 2>&1
 
+echo "==> Stopping OpenClaw ..."
 docker_compose down
 
 popd > /dev/null 2>&1
